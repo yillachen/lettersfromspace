ad52bf80f3045d530882bf4a7e82aa4d3824d2cb
yillachen <chn.yilla@gmail.com>
2022-02-23 15:01:22 -0500

    bug: unable to render photos even though console shows the urls.

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,64 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
+import Image from "next/image";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/posts";
+import { getlast9Days } from "./api/ninePhotos";
 import Link from "next/link";
 import Date from "../components/date";
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
+export default function Home() {
+  const [photos, setPhotos] = useState([]);
 
-export default function Home({ allPostsData }) {
+  useEffect(() => {
+    async function getData() {
+      let response = await getlast9Days();
+      response = response.filter((pic) => pic.media_type === "image");
+      setPhotos(response);
+      console.log(`response: `, response);
+    }
+    getData();
+    return () => "Unmounting";
+  }, []);
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        {photos.length > 0
+          ? photos.map((photo) => {
+              <>
+                <p>{photo.title}</p>
+                <Image
+                  width="675px"
+                  height="675px"
+                  id="myImg"
+                  src={photo.url}
+                  alt={photo.title}
+                />
+              </>;
+              // photo.media_type === "image" ? (
+
+              //   <>
+              //     <p>Testing</p>
+              //     <Image
+              //   priority
+              //   src={photo.hdurl}
+              //   height={200}
+              //   width={200}
+              //   alt={photo.title}
+              // />
+              //   </>
+              // ) : (
+              //   "Videos are not supported yet."
+              // );
+            })
+          : "Loading..."}
+      </section>
+
+      {/* <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
@@ -43,7 +81,7 @@ export default function Home({ allPostsData }) {
             </li>
           ))}
         </ul>
-      </section>
+      </section> */}
     </Layout>
   );
 }
